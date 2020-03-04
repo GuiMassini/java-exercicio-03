289291810dea55bf3114b8c740fdc7657aa25ec2
Matuk <mateusmatuk@fai-mg.br>
2020-03-04 20:55:01 -0300

    Segundo commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Start");
+				
+		char [] palavra = new char[20];
+		String entrada = "Hello World";
+
+		System.out.println("Hello World");
+		
+		for(int i=0;i<entrada.length();i++){
+			palavra[i] = entrada.charAt(i);
+		}
+		
+		for(int i=entrada.length();i>0;i--){
+			System.out.print(palavra[i-1]);
+		}
+	   
 
 	}
 
